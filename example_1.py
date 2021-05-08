06ef78a0fce8f1b12e58b45a943fc8b4a247904b
Hassan <atwih@usi.ch>
2021-05-08 18:31:41 +0200

    Move Push Down Ref

--- example_1.py
+++ example_1.py
@@ -1,5 +1,10 @@
 from example import *
-
+"""Move Method
+obj = a + c and obj = a + func(c) are matched due to element sorting.
+"""
 class example_class_child(example_class):
 	def methodC(self):
         print("Hello World")
+    
+	def methodB(self):
+		obj = a + func(c)
