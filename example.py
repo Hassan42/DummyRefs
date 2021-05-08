ef1466d9c8470b90a6bb862d194b5c8460206e86
Hassan <atwih@usi.ch>
2021-05-08 19:26:38 +0200

    Example Method Added: No Refs

--- example.py
+++ example.py
@@ -1,4 +1,6 @@
 class example_class:
-	def methodA(self):
-		print(0)
-		total = 1 + 2 + 3
+    def methodB(self):
+        print(0)
+        if True:
+            x = 1 + 2 + 3
+        print("Extract_Rename")
