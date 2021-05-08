841be8adcd29a64d15403b7c27468b2b183276ef
Hassan <atwih@usi.ch>
2021-05-08 17:06:51 +0200

    Added Class C: No REFs

--- example.py
+++ example.py
@@ -1,12 +1,7 @@
-"""Extract Method & Extract Var
-methodB() was extracted from methodA(), including a nested extract var refactoring (ext_obj).
-"""
+class C:
+    a: float
+    b: float
+    c: float = field(init=False)
 
-class example:
-        def methodA(self):
-            self.methodB()
-            
-        def methodB(self):
-            ext_obj = obj1.func1()
-            obj = ext_obj.func2().func3()
-            print("End")
+    def __post_init__(self):
+        self.c = self.a + self.b
