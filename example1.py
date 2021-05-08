a07d359403e94c63a25791e0e609e1d20640c03a
Hassan <atwih@usi.ch>
2021-05-08 19:01:11 +0200

    Move Push Down Ref

--- example1.py
+++ example1.py
@@ -1,5 +1,9 @@
 from example import *
 
 class example_class_child(example_class):
+
 	def methodC(self):
-        print("Hello World")
+		print("Hello World")
+
+	def methodB(self):
+		obj = a + func(c)
