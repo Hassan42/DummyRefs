42f388e0abfba645b163475b81220331e6680102
Hassan <atwih@usi.ch>
2021-05-08 19:10:52 +0200

    Added New Module: No REFs

--- example.py
+++ example.py
@@ -2,3 +2,6 @@ class example_class:
 	def methodA(self):
 		print(0)
 		total = 1 + 2 + 3
+
+	def methodB(self):
+		obj = a + c
--- example1.py
+++ example1.py
@@ -1,9 +1,5 @@
 from example import *
 
 class example_class_child(example_class):
-
-	def methodC(self):
-		print("Hello World")
-
-	def methodB(self):
-		obj = a + func(c)
+        def methodC(self):
+            print("Hello World")
