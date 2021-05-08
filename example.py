40ba722e1f740b724b1e8a6845b1c1f9dfbc748c
Hassan <atwih@usi.ch>
2021-05-08 17:00:36 +0200

    Extract Method & Extract Var REFs

--- example.py
+++ example.py
@@ -1,4 +1,12 @@
+"""Extract Method & Extract Var
+methodB() was extracted from methodA(), including a nested extract var refactoring (ext_obj).
+"""
+
 class example:
         def methodA(self):
-            obj = obj1.func1().func2().func3()
+            self.methodB()
+            
+        def methodB(self):
+            ext_obj = obj1.func1()
+            obj = ext_obj.func2().func3()
             print("End")
