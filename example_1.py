e605abea39ce071c75302c761de689128aab8cb2
Hassan <atwih@usi.ch>
2021-05-08 18:43:12 +0200

    Added New Module: No REFs

--- example_1.py
+++ example_1.py
@@ -1,10 +1,5 @@
 from example import *
-"""Move Method
-obj = a + c and obj = a + func(c) are matched due to element sorting.
-"""
+
 class example_class_child(example_class):
 	def methodC(self):
         print("Hello World")
-    
-	def methodB(self):
-		obj = a + func(c)
