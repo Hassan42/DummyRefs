e6cca4f29396d92296bbe204746015c7fbf11075
Hassan <atwih@usi.ch>
2021-05-08 19:29:01 +0200

    Extract and Rename: REFs

--- example.py
+++ example.py
@@ -1,6 +1,12 @@
+"""Rename & Extract Method
+With the information of the renaming methodB to methodC, we could deduce that methoD was extracted from methodB.
+"""
 class example_class:
-    def methodB(self):
+    def methodC(self):
         print(0)
         if True:
             x = 1 + 2 + 3
+        self.methodD()
+
+    def methodD(self):
         print("Extract_Rename")
